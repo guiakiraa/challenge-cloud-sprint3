ae7941e1ee5bf6cd9770526f60984d7226bd0649
Guilherme Akira Nakamatsu <162456149+guiakiraa@users.noreply.github.com>
2025-09-26 17:33:10 -0300

    Update V1__criando_tabelas.sql

--- src/main/resources/db/migration/V1__criando_tabelas.sql
+++ src/main/resources/db/migration/V1__criando_tabelas.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS FUNCAO (
-    id bigint AUTO_INCREMENT,
+CREATE TABLE FUNCAO (
+    id bigint IDENTITY(1,1),
     nome VARCHAR(20) CHECK (nome IN ('ADMIN', 'GERENTE')),
     primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS USUARIO (
-    id bigint AUTO_INCREMENT,
+CREATE TABLE USUARIO (
+    id bigint IDENTITY(1,1),
     img_perfil varchar(255),
     nome_perfil varchar(255),
     senha varchar(255),
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS USUARIO (
     primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS USUARIO_FUNCAO_TAB(
+CREATE TABLE USUARIO_FUNCAO_TAB(
     id_funcao bigint not null,
     id_usuario bigint not null,
     primary key (id_funcao, id_usuario),
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS USUARIO_FUNCAO_TAB(
     constraint FK_usuario_funcao_user foreign key (id_usuario) references USUARIO(id)
 );
 
-CREATE TABLE IF NOT EXISTS endereco (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE endereco (
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
     logradouro VARCHAR(255) NOT NULL,
     numero INT NOT NULL,
     bairro VARCHAR(100) NOT NULL,
@@ -32,15 +32,15 @@ CREATE TABLE IF NOT EXISTS endereco (
     complemento VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS filial (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE filial (
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     fk_endereco BIGINT NOT NULL,
     CONSTRAINT FK_filial_endereco FOREIGN KEY (fk_endereco) REFERENCES endereco(id)
 );
 
-CREATE TABLE IF NOT EXISTS moto (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE moto (
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
     placa VARCHAR(7) NOT NULL,
     ano INT NOT NULL,
     modelo VARCHAR(50) NOT NULL,
@@ -49,15 +49,15 @@ CREATE TABLE IF NOT EXISTS moto (
     CONSTRAINT FK_moto_filial FOREIGN KEY (fk_filial) REFERENCES filial(id)
 );
 
-CREATE TABLE IF NOT EXISTS funcionario (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE funcionario (
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     fk_filial BIGINT NOT NULL,
     CONSTRAINT FK_funcionario_filial FOREIGN KEY (fk_filial) REFERENCES filial(id)
 );
 
-CREATE TABLE IF NOT EXISTS localizacao (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE localizacao (
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
     pontox DOUBLE NOT NULL,
     pontoy DOUBLE NOT NULL,
     data_hora TIMESTAMP NOT NULL,
